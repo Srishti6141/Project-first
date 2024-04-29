63bd2f29939f2167639ac3077d443c2c0c11fa17
Srishti6141 <srishtipandey@gmail.com>
2024-04-29 22:31:26 +0530

    changed

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-$(window).scroll(function(e) {
-        
-    var distanceScrolled = $(this).scrollTop();
-  
-    if( distanceScrolled == 200){
-       $('#image1').css({"display" : "none"})
-    }
-  });
+
 function locomotiveAnimation(){
     gsap.registerPlugin(ScrollTrigger);
 
